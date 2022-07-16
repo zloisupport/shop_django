d066bbc60b404eb47e866dc72d8fcc4d283264fa
zloisupport <kr0t77@yandex.ru>
2022-07-16 22:24:15 +0600

    add templates

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from .models import *
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField,ModelForm,ValidationError
+
+from PIL import Image
 
 
+class NotebookAdminForm(ModelForm):
+    MIN_RESOLUTION = (400, 400)
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['image'].help_text = 'Загружайте изображение с мин разрешением {}x{}'.format(*self.MIN_RESOLUTION)
+
+    def clean_image(self):
+        image =self.cleaned_data['image']
+        img = Image.open(image)
+        min_height , min_width = self.MIN_RESOLUTION
+        if (img.width <min_width or img.height <min_height):
+            raise ValidationError('Изображение меньше установленного')
+        return  image
 class NotebookAdmin(admin.ModelAdmin):
+    form = NotebookAdminForm
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
             return ModelChoiceField(Category.objects.filter(slug='notebook'))
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,1 +1,5 @@
-urlpatterns = []
+from .views import test_view
+from django.urls import path, include
+urlpatterns = [
+    path('',test_view,name='base')
+]
