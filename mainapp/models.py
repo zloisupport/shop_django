e9ecfed4a257b6e07623c60e9b046c405068e839
zloisupport <kr0t77@yandex.ru>
2022-07-16 22:24:00 +0600

    checking img resolution

--- mainapp/models.py
+++ mainapp/models.py
@@ -15,6 +15,29 @@ User = get_user_model()
 # customer
 # Specification
 
+class LatestProductManager:
+
+    @staticmethod
+    def get_products_for_main_page(self,*args,**kwargs):
+        with_respect_to = kwargs.get('with_respect_to')
+        products= []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_products =  ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_products)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(
+                        products, key=lambda x:x.__class__._meta.model_name.startswith(with_respect_to), reverse=True
+                    )
+        return products
+
+
+class LatestProducts:
+    objects = LatestProductManager()
+
 class Category(models.Model):
     name = models.CharField(max_length=255, verbose_name='Категории')
     slug = models.SlugField(unique=True)
